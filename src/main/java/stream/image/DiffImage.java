75806adad6b99e27e70d1e15241df31efb72bda0
reiseleiter <matthias-schulte@web.de>
2012-11-09 17:42:22 +0100

    first try ...

--- src/main/java/stream/image/DiffImage.java
+++ src/main/java/stream/image/DiffImage.java
@@ -1,19 +1,22 @@
 package stream.image;
 
+import java.awt.Color;
+
 import stream.Data;
 
 public class DiffImage extends AbstractImageProcessor {
 	
 	ImageRGB lastImage = new ImageRGB(0, 0);
-
-	public DiffImage() {
-	// TODO Auto-generated constructor stub
-	}
 	
 	@Override
 	public Data process(Data item, ImageRGB img) {
 		
-		ImageRGB diffImage = img;
+		ImageRGB diffImage = new ImageRGB(img.getHeight(), img.getWidth());
+		for (int i=0; i<diffImage.getWidth(); i++) {
+			for (int j=0; j<diffImage.getHeight(); j++) {
+				diffImage.setRGB(i, j, img.getRGB(i, j));
+			}
+		}
 		
 		if (diffImage.getHeight() == lastImage.getHeight() && diffImage.getWidth() == lastImage.getWidth()) {
 			for (int i=0; i<diffImage.getWidth(); i++) {
@@ -30,7 +33,22 @@ public class DiffImage extends AbstractImageProcessor {
 					int gnew = (rgbnew >> 8) & 0xFF;
 					int bnew = rgbnew & 0xFF; 
 					
-					int rgbdiff = Math.abs(rold - rnew) <<16 + Math.abs(gold - gnew) << 8 + Math.abs(bold - bnew);
+					int rdiff = Math.abs(rold - rnew);
+					int gdiff = Math.abs(gold - gnew);
+					int bdiff = Math.abs(bold - bnew);
+					
+					rdiff = 255 - rdiff;
+					gdiff = 255 - gdiff;
+					bdiff = 255 - bdiff;
+					
+					if (rdiff < 20) { rdiff = 0; }
+					if (gdiff < 20) { gdiff = 0; }
+					if (bdiff < 20) { bdiff = 0; }
+					
+					//int rgbdiff = rdiff * 65536 + gdiff * 265 + bdiff;
+					int rgbdiff = rdiff;
+					rgbdiff = (rgbdiff << 8) + gdiff;
+					rgbdiff = (rgbdiff << 8) + bdiff; 
 					
 					diffImage.setRGB(i, j, rgbdiff);
 				}
