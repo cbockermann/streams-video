f0f61c3dc9d7185bfc5220999f08933a2b600715
reiseleiter <matthias-schulte@web.de>
2012-11-27 10:10:07 +0100

    Testing the smoothing operator still not completed

---
+++ src/test/java/stream/io/CoffeeTest.java
@@ -0,0 +1,32 @@
+package stream.io;
+
+/**
+ * 
+ */
+
+import java.net.URL;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author chris
+ * 
+ */
+public class CoffeeTest {
+
+	static Logger log = LoggerFactory.getLogger(CoffeeTest.class);
+
+	@Test
+	public void test() throws Exception {
+		URL url = CoffeeTest.class
+				.getResource("/border-detection-stream.xml");
+		log.info("Starting MJpegImageStream-test from {}", url);
+		stream.run.main(url);
+	}
+
+	public static void main(String[] args) throws Exception {
+		new CoffeeTest().test();
+	}
+}
