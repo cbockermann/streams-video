b9b8faad365d40ddf2195d7f24fcfa6fe3722799
h_blom <hendrik.blom@udo.edu>
2012-12-14 14:51:30 +0100

    some sparkles of magic....

--- src/main/java/stream/laser/LaserTracker.java
+++ src/main/java/stream/laser/LaserTracker.java
@@ -42,10 +42,12 @@ public class LaserTracker extends AbstractImageProcessor {
 	protected String output;
 	protected boolean skipWithoutPoint = false;
 	DatagramSocket socket;
-	protected  String address;
+	protected String address;
 	protected int port = 9105;
-	protected  InetAddress addr;
-	protected  DatagramPacket packet;
+	protected InetAddress addr;
+	protected DatagramPacket packet;
+	protected int evalMagic;
+	protected int initialMagic;
 
 	public LaserTracker() {
 		laserImage = null;
@@ -54,6 +56,8 @@ public class LaserTracker extends AbstractImageProcessor {
 		searchSize = 20;
 		threshold = 20;
 		output = imageKey;
+		evalMagic=0;
+		initialMagic=0;
 	}
 
 	/**
@@ -184,7 +188,6 @@ public class LaserTracker extends AbstractImageProcessor {
 			initialPoint = getInitialPoint(img);
 			if (initialPoint != null) {
 				initialRGB = img.getRGB(initialPoint.x, initialPoint.y);
-				markLaserPointer(initialPoint, img, 0, 255, 0);
 				log.info(
 						"********************* found initial point {} ***************************",
 						initialPoint);
@@ -192,6 +195,24 @@ public class LaserTracker extends AbstractImageProcessor {
 			item.put(output, img);
 			return item;
 		}
+		if(initialMagic<3){
+			Point cp = getInitialPoint(img);
+			if(cp==null){
+			}else{
+			double magicDist = dist(initialPoint, cp);
+			
+			if(magicDist<100)
+				initialMagic++;
+			else{
+				initialMagic=0;
+				initialPoint=null;
+			}
+			}
+			item.put(output, img);
+			return item;
+		}
+		
+		
 		Point evalPoint = evaluateLaserPointer(initialPoint, initialRGB, img);
 		if (evalPoint != null) {
 			// log.info("Found laserPointer");
@@ -213,29 +234,31 @@ public class LaserTracker extends AbstractImageProcessor {
 			}
 			return item;
 		}
-
-		if (skipWithoutPoint)
-			return null;
+//
+//		if (skipWithoutPoint)
+//			return null;
 
 		// log.info("can't find laserPointer");
 		initialPoint = null;
 		initialRGB = -1;
+		initialMagic=0;
 		return item;
 	}
 
-	private Point evaluateLaserPointer(Point p, int oldRGB, ImageRGB img) {
+	private Point evaluateLaserPointer(Point ep, int oldRGB, ImageRGB img) {
 		int size = searchSize;
 
-		int count = 3;
+		int count = 10;
 		Point[] points = new Point[count];
 
 		int minThreshold = threshold;
 
-		int minx = (p.x - size > 0) ? p.x - size : 0;
-		int maxx = (p.x + size > img.width - 1) ? img.width - 1 : (p.x + size);
-		int miny = (p.y - size > 0) ? p.y - size : 0;
-		int maxy = (p.y + size > img.height - 1) ? img.height - 1
-				: (p.y + size);
+		int minx = (ep.x - size > 0) ? ep.x - size : 0;
+		int maxx = (ep.x + size > img.width - 1) ? img.width - 1
+				: (ep.x + size);
+		int miny = (ep.y - size > 0) ? ep.y - size : 0;
+		int maxy = (ep.y + size > img.height - 1) ? img.height - 1
+				: (ep.y + size);
 
 		int[] pixels = img.pixels;
 		for (int x = minx; x < maxx; x++) {
@@ -257,11 +280,18 @@ public class LaserTracker extends AbstractImageProcessor {
 				}
 			}
 		}
-		
 
-//		return findMinDist(p,points);
-//		return average(p,points);
-		return weightedAverage(p,points);		
+		// return findMinDist(p,points);
+		// return average(p,points);
+		Point p = weightedAverage(ep, points);
+		if (p == null) {
+			if (evalMagic < 3) {
+				evalMagic++;
+				return ep;
+			}
+		}
+		evalMagic=0;
+		return p;
 	}
 
 	private Point getInitialPoint(ImageRGB img) {
@@ -300,29 +330,27 @@ public class LaserTracker extends AbstractImageProcessor {
 		color = (color << 8) + b;
 
 		int idx = (y - 5) * img.width + x - 5;
-		if (idx < img.pixels.length && idx>=0)
+		if (idx < img.pixels.length && idx >= 0)
 			img.pixels[idx] = color;
 
 		idx = (y + 5) * img.width + x - 5;
-		if (idx < img.pixels.length && idx>=0)
+		if (idx < img.pixels.length && idx >= 0)
 			img.pixels[idx] = color;
 
 		idx = (y - 5) * img.width + x + 5;
-		if (idx < img.pixels.length && idx>=0)
+		if (idx < img.pixels.length && idx >= 0)
 			img.pixels[idx] = color;
 
 		idx = (y + 5) * img.width + x + 5;
-		if (idx < img.pixels.length && idx>=0)
+		if (idx < img.pixels.length && idx >= 0)
 			img.pixels[idx] = color;
 	}
 
 	public double dist(Point p, Point q) {
 		return p.distance(q.getX(), q.getY());
 	}
-	
-	
-	
-	private Point findMinDist(Point p,Point[] points){
+
+	private Point findMinDist(Point p, Point[] points) {
 		int minp = points.length;
 		double dist = 0.0d;
 		double minDist = Double.MAX_VALUE;
@@ -337,50 +365,50 @@ public class LaserTracker extends AbstractImageProcessor {
 				minDist = dist;
 			}
 		}
-		return (minp==points.length)?null:points[minp];
+		return (minp == points.length) ? null : points[minp];
 	}
-	
-	private Point average(Point ep,Point[] points){
-		int x=0;
-		int y=0;
-		int count =0;
-		for(Point p: points){
-			if(p!=null){
-			x+=p.x;
-			y+=p.y;
-			}else
+
+	private Point average(Point ep, Point[] points) {
+		int x = 0;
+		int y = 0;
+		int count = 0;
+		for (Point p : points) {
+			if (p != null) {
+				x += p.x;
+				y += p.y;
+			} else
 				count++;
 		}
-		if(count==points.length)
+		if (count == points.length)
 			return null;
-		x/=points.length;
-		y/=points.length;
-		Point r = new Point(x,y);
-		log.info("found point {}",r);
+		x /= points.length;
+		y /= points.length;
+		Point r = new Point(x, y);
+		log.info("found point {}", r);
 		return r;
 	}
-	
-	private Point weightedAverage(Point ep,Point[] points){
-		int x=0;
-		int y=0;
-		int count =0;
-		double dist=0;
-		for(Point p: points){
-			if(p!=null){
-			double d=dist(ep, p);
-			dist+=(searchSize-d);
-			x+=  p.x * (searchSize-d);
-			y+= p.y * (searchSize-d);
-			}else
+
+	private Point weightedAverage(Point ep, Point[] points) {
+		int x = 0;
+		int y = 0;
+		int count = 0;
+		double dist = 0;
+		for (Point p : points) {
+			if (p != null) {
+				double d = dist(ep, p);
+				dist += (searchSize - d);
+				x += p.x * (searchSize - d);
+				y += p.y * (searchSize - d);
+			} else
 				count++;
 		}
-		if(count==points.length)
+		if (count == points.length)
 			return null;
-		x/=dist;
-		y/=dist;
-		Point r = new Point(x,y);
-		log.info("found point {}",r);
+		x /= dist;
+		y /= dist;
+		Point r = new Point(x, y);
+		log.info("found point {}", r);
 		return r;
 	}
-	
+
 }
