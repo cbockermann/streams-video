12a0200f4ad6245a2649b99f820d0f07ea50390a
h_blom <hendrik.blom@udo.edu>
2013-01-16 14:30:16 +0100

    active weighted average for evalLaserPoint

--- src/main/java/stream/laser/LaserTrackerGreenNew.java
+++ src/main/java/stream/laser/LaserTrackerGreenNew.java
@@ -312,8 +312,8 @@ public class LaserTrackerGreenNew extends AbstractImageProcessor {
 
 		// return findMinDist(p,points);
 		// return average(p,points);
-//		Point p = weightedAverage(ep, points);
-		Point p=points[0];
+		Point p = weightedAverage(ep, points);
+//		Point p=points[0];
 //		if (p == null) 
 //				return ep;
 		return p;
